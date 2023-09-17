9c087754c8fe5d74fc1b820d024da14b4a27b43e
m-uson <usonibrahimovic@gmail.com>
2023-09-18 00:28:49 +0600

    addf

--- src/components/ServiceCard/ServiceCard.jsx
+++ src/components/ServiceCard/ServiceCard.jsx
@@ -6,19 +6,30 @@ import {
   Typography,
 } from "@mui/material";
 import img from "../../assets/image.png";
+import Partner1 from "../../assets/imgs/partners/shoro.svg";
+
+import "../Partners/partners.scss";
+import Swiper, { Autoplay, Pagination } from "swiper";
+import { SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/autoplay";
 
 const ServiceCard = ({ service }) => {
   return (
-    <div className="flex flex-wrap justify-between gap-y-14 w-full">
+    <div className="flex flex-wrap gap-20 w-full">
       {service?.map((item, index) => (
         <Card key={index} sx={{ maxWidth: 270 }}>
           <CardActionArea>
-            <CardMedia
-              component="img"
-              height="140"
-              image={img}
-              alt="green iguana"
-            />
+            <div className="partners">
+              <CardMedia
+                component="img"
+                height="140"
+                image={img}
+                alt="green iguana"
+              />
+            </div>
             <CardContent>
               <Typography gutterBottom variant="h5" component="div">
                 {item.nameRu}
